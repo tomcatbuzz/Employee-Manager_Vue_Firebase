40bb45254a0b2b9baa5d0e5d3bbccf8518739237
tomcatbuzz <tomcatbuzz@yahoo.com>
2018-05-05 23:03:35 -0400

    added firebaseConfigExample.js

---
+++ src/components/firebaseConfigExample.js
@@ -0,0 +1,10 @@
+// Add your Firebase Web App Info in this File and Rename the File to firebaseConfig.js
+
+export default {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
+}
